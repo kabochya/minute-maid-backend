2b86fcd6ad1e29d539670f8918fbfca843aa6eeb
TianxiongWang <nickwang57@gmail.com>
2017-09-24 02:21:18 -0400

    change bucket

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 class Config(object):
     """Parent configuration class"""
     DEBUG = False
-    BUCKET_ID = "xxnet-172504.appspot.com"
-    PROJECT_ID = "xxnet-172504"
+    BUCKET_ID = "mhacks-audio"
+    PROJECT_ID = "celestial-tract-180903"
     MONGO_DBNAME = 'mhacks'
     MONGO_URI = 'mongodb://localhost:27017/mhacks'
 
