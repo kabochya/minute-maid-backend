7a42c0a349c293fc7e0ed8174ee9fcce139afce9
TianxiongWang <nickwang57@gmail.com>
2017-09-24 01:16:45 -0400

    import pymongo

--- mhacks.py
+++ mhacks.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
 
 from flask import Flask
+from flask_pymongo import PyMongo
 from flask import request
 import os
 from config import app_config
